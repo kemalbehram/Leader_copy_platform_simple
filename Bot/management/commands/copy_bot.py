6da2f1b8c3c388bbbcd3872e2729fe2e671b8481
Osvald <59208771+osvaldai@users.noreply.github.com>
2023-04-28 08:20:09 +0300

    1

--- Bot/management/commands/copy_bot.py
+++ Bot/management/commands/copy_bot.py
@@ -32,6 +32,93 @@ class Command(BaseCommand):
         t = threading.Thread(target=close_manually_position)
         t.start()
         while True:
+            users = Users.objects.filter(subs_active=True)
+
+            # получаем активные ордера
+            sig_ord = Signal.objects.filter(is_active=True, status=True)
+
+            # сравниваем истекли срок годности ордера
+            try:
+                for order_s in sig_ord:
+                    date_end = order_s.upd
+
+                    now = datetime.now()
+
+                    a = now - parser.parse(date_end)
+                    delta = a.seconds / 60
+                    # если срок годности ордера больше 1 минут, то получаем информацию об открытой позиции
+                    # и закрываем её
+                    print('DELTA = ' + str(round(delta, 2)) + f' {order_s.symbol}/ trader {order_s.name_trader}')
+                    if delta >= 0.58:
+                        try:
+                            t = threading.Thread(target=order_close, args=(order_s, admin))
+                            t.start()
+                        except Exception as e:
+                            # The name of your app and dyno
+                            app_name = 'aws copy-trade-leaderboard'
+                            debug(
+                                e, app_name
+                            )
+                            exc_type, exc_obj, exc_tb = sys.exc_info()
+                            print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
+                        for user in users:
+                            sleep(0.4)
+                            try:
+                                t = threading.Thread(target=order_close, args=(order_s, user))
+                                t.start()
+                            except Exception as e:
+                                # The name of your app and dyno
+                                app_name = 'aws copy-trade-leaderboard'
+                                debug(
+                                    e, app_name
+                                )
+                                exc_type, exc_obj, exc_tb = sys.exc_info()
+                                print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
+
+                        Signal.objects.filter(symbol=order_s.symbol, is_active=True).update(is_active=False)
+
+            except Exception as e:
+                app_name = 'aws copy-trade-leaderboard'
+                debug(
+                    e, app_name
+                )
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
+
+            # sleep(1)
+            # получаем ордера со статусом Фолс
+            signals = Signal.objects.filter(status=False)
+            # sleep(0.2)
+            # получаем айди трейдеров и
+            for signal in signals:
+                sleep(0.3)
+                try:
+                    t = threading.Thread(target=open_position, args=(signal, admin))
+                    t.start()
+                except Exception as e:
+                    # The name of your app and dyno
+                    app_name = 'aws copy-trade-leaderboard'
+                    debug(
+                        e, app_name
+                    )
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
+                for user in users:
+                    sleep(0.5)
+                    try:
+                        t = threading.Thread(target=open_position, args=(signal, user))
+                        t.start()
+                    except Exception as e:
+                        # The name of your app and dyno
+                        app_name = 'aws copy-trade-leaderboard'
+                        debug(
+                            e, app_name
+                        )
+                        exc_type, exc_obj, exc_tb = sys.exc_info()
+                        print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
+            # обновляем статус сигналов
+            signals.update(status=True)
+            # sleep(1)
             sleep(10)
             traders = Traders.objects.filter(is_active=True)
             for trade in traders:
@@ -51,69 +138,6 @@ class Command(BaseCommand):
 
                     exc_type, exc_obj, exc_tb = sys.exc_info()
                     print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
-            # sleep(1)
-            # получаем ордера со статусом Фолс
-            signals = Signal.objects.filter(status=False)
-            # sleep(0.2)
-            # получаем айди трейдеров и
-            users = Users.objects.filter(subs_active=True)
-
-            t = threading.Thread(target=open_position_users, args=(signals, admin, users))
-            t.start()
-            # обновляем статус сигналов
-            signals.update(status=True)
-            # sleep(1)
-            # получаем активные ордера
-            sig_ord = Signal.objects.filter(is_active=True, status=True)
-
-            # сравниваем истекли срок годности ордера
-            try:
-                for order_s in sig_ord:
-                    date_end = order_s.upd
-
-                    now = datetime.now()
-
-                    a = now - parser.parse(date_end)
-                    delta = a.seconds / 60
-                    # если срок годности ордера больше 1 минут, то получаем информацию об открытой позиции
-                    # и закрываем её
-                    print('DELTA = ' + str(round(delta, 2)) + f' {order_s.symbol}/ trader {order_s.name_trader}')
-                    if delta >= 0.58:
-                        t = threading.Thread(target=close_positions_users, args=(order_s, admin, users))
-                        t.start()
-
-                        # if order_s.side == 'BUY':
-                        #     # msg = f'<b>📳 TRADE CLOSED 📳</b>\n\n' \
-                        #     #       f'🥷🏾Trader: <b>{order_s.name_trader}</b>\n' \
-                        #     #       f'💎Crypto: <b>{order_s.symbol}</b>\n' \
-                        #     #       f'🔵Trade: <b>BUY</b> (LONG)\n\n' \
-                        #     #       f'📊Marketprice: <b>{order_s.mark_price}</b>\n' \
-                        #     #       f'💹PNL: <b>{round(float(order_s.roe), 2)}%</b>\n\n'
-                        #     # try:
-                        #     #     # bots.send_message(my_id, msg, parse_mode='HTML')
-                        #     # except:
-                        #     #     pass
-                        # else:
-                        #     msg = f'<b>📳 TRADE CLOSED 📳</b>\n\n' \
-                        #           f'🥷🏾Trader: <b>{order_s.name_trader}</b>\n' \
-                        #           f'💎Crypto: <b>{order_s.symbol}</b>\n' \
-                        #           f'🔴Trade: <b>Sell</b> (SHORT)\n\n' \
-                        #           f'📊Marketprice: <b>{order_s.mark_price}</b>\n' \
-                        #           f'💹PNL: <b>{round(float(order_s.roe), 2)}%</b>\n\n'
-                        # try:
-                        #     bots.send_message(my_id, msg, parse_mode='HTML')
-                        # except:
-                        #     pass
-
-                        Signal.objects.filter(symbol=order_s.symbol, is_active=True).update(is_active=False)
-
-            except Exception as e:
-                app_name = 'aws copy-trade-leaderboard'
-                debug(
-                    e, app_name
-                )
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                print(str(e) + 'line = ' + str(exc_tb.tb_lineno))
             # sleep(15)
             # try:
             #     check_users()
